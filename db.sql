f6db44bfd01d92addb432fd4347745cb0dd0cf3d
Daniel <algo@mail.com>
2021-10-07 15:45:51 -0500

    Add style

--- db.sql
+++ db.sql
@@ -9,5 +9,11 @@ CREATE TABLE cancion (
 
 INSERT INTO cancion VALUES (2,'Paranoid','BLACK SABBATH','https://youtu.be/0qanF-91aJo','https://pbs.twimg.com/media/EiKBFiTXYAM8E4T.jpg');
 INSERT INTO cancion VALUES (2,'N.I.B','BLACK SABBATH','https://youtu.be/nJYdDmUvgok','https://i1.sndcdn.com/artworks-000575098358-mab9q4-t500x500.jpg');
+INSERT INTO cancion VALUES (2,'Paranoid1','BLACK SABBATH','https://youtu.be/0qanF-91aJo','https://pbs.twimg.com/media/EiKBFiTXYAM8E4T.jpg');
+INSERT INTO cancion VALUES (2,'N.I.B1','BLACK SABBATH','https://youtu.be/nJYdDmUvgok','https://i1.sndcdn.com/artworks-000575098358-mab9q4-t500x500.jpg');
+INSERT INTO cancion VALUES (2,'Paranoid2','BLACK SABBATH','https://youtu.be/0qanF-91aJo','https://pbs.twimg.com/media/EiKBFiTXYAM8E4T.jpg');
+INSERT INTO cancion VALUES (2,'N.I.B2','BLACK SABBATH','https://youtu.be/nJYdDmUvgok','https://i1.sndcdn.com/artworks-000575098358-mab9q4-t500x500.jpg');
+INSERT INTO cancion VALUES (2,'Paranoid3','BLACK SABBATH','https://youtu.be/0qanF-91aJo','https://pbs.twimg.com/media/EiKBFiTXYAM8E4T.jpg');
+INSERT INTO cancion VALUES (2,'N.I.B3','BLACK SABBATH','https://youtu.be/nJYdDmUvgok','https://i1.sndcdn.com/artworks-000575098358-mab9q4-t500x500.jpg');
 
 SELECT * FROM cancion;
